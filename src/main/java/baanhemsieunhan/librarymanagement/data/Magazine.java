7a982f1828c6c817797da9463201eb86fade399f
LongMixi0401 <162767627+LongMixi0401@users.noreply.github.com>
2024-09-28 15:48:58 +0700

    [LongNB] update Document

---
+++ src/main/java/baanhemsieunhan/librarymanagement/data/Magazine.java
@@ -0,0 +1,38 @@
+package baanhemsieunhan.librarymanagement.data;
+
+class Magazine extends Document {
+    private int issueNumber;
+    private String month;
+
+    public Magazine() {}
+
+    public Magazine(String documentID, String title, String publisher, int publicationYear, int numberAll, int numberAvailable, int issueNumber, String month) {
+        super(documentID, title, publisher, publicationYear, numberAll, numberAvailable);
+        this.issueNumber = issueNumber;
+        this.month = month;
+    }
+
+    public int getIssueNumber() {
+        return issueNumber;
+    }
+
+    public void setIssueNumber(int issueNumber) {
+        this.issueNumber = issueNumber;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + "\n" +
+                "Issue Number: " + issueNumber + "\n" +
+                "Month: " + month;
+    }
+}
+
