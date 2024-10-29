f8415ed6212afa5b56d22f3b2799bfa8895cbe87
LongMixi0401 <162767627+LongMixi0401@users.noreply.github.com>
2024-10-29 20:27:10 +0700

    [LongNB] fix SQL

---
+++ src/main/java/librarymanagement/data/Constant.java
@@ -0,0 +1,7 @@
+package librarymanagement.data;
+
+public class Constant {
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "longmixi0401";
+    public static final String URL = "jdbc:mysql://localhost:3306/library";
+}
