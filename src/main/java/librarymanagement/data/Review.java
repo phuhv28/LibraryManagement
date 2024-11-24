7aa3e7903958703e748ff07d6e269754117d93fb
LongMixi0401 <162767627+LongMixi0401@users.noreply.github.com>
2024-11-24 22:33:13 +0700

    [LongNB] add class Review

---
+++ src/main/java/librarymanagement/data/Review.java
@@ -0,0 +1,47 @@
+package librarymanagement.data;
+
+public class Review {
+    String username;
+    String docID;
+    String comment;
+    int rating;
+
+    public Review(String username, String docID, String comment, int rating) {
+        this.username = username;
+        this.docID = docID;
+        this.comment = comment;
+        this.rating = rating;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDocID() {
+        return docID;
+    }
+
+    public void setDocID(String docID) {
+        this.docID = docID;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
---
+++ src/main/java/librarymanagement/data/ReviewService.java
@@ -0,0 +1,56 @@
+package librarymanagement.data;
+
+import librarymanagement.UserAuth.AccountService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewService {
+    SQLiteInstance sqLiteInstance = new SQLiteInstance();
+
+    public ReviewService() {
+    }
+
+    public boolean checkIfUserHasCommentToDocument(String username, String docID) {
+        String sql = "SELECT userID FROM Review WHERE username = " + username + " AND docID = " + docID;
+        List<List<Object>> lists = sqLiteInstance.findWithSQL(sql, new Object[]{"username"}, "username");
+        return !lists.isEmpty();
+    }
+
+    public boolean addReview(String username, String docID, int rating, String comment) {
+        if (checkIfUserHasCommentToDocument(username, docID)) {
+            return false;
+        }
+        if (docID.charAt(0) == 'B') {
+            BookService bookService = new BookService();
+            Book book = bookService.findDocumentById(docID);
+            book.setAverageRating((book.getAverageRating() * book.getRatingsCount() + (double) rating)
+                    / (book.getRatingsCount() + 1));
+            book.setRatingsCount(book.getRatingsCount() + 1);
+            bookService.updateDocument(book);
+        } else if (docID.charAt(0) == 'M') {
+            //TODO
+        } else if (docID.charAt(0) == 'T') {
+            //TODO
+        }
+        sqLiteInstance.insertRow("review", username, docID, comment, rating);
+        return true;
+    }
+
+    public boolean addReviewForCurrentAccount(String docID, String comment, int rating) {
+        String username = AccountService.getInstance().getCurrentAccount().getUsername();
+        return addReview(username, docID, rating, comment);
+    }
+
+    public List<Review> getAllReviewsInDocument(String docID) {
+        List<Review> reviews = new ArrayList<>();
+        List<List<Object>> lists = sqLiteInstance.find("Review", "docID", docID, "username", "rating", "comment");
+        for (List<Object> list : lists) {
+            String username = (String) list.get(0);
+            int rating = (int) list.get(1);
+            String comment = (String) list.get(2);
+            reviews.add(new Review(username, docID, comment, rating));
+        }
+        return reviews;
+    }
+}
